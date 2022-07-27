26bc9c54511cfe368aac6d4d854af456e8829d6a
zachary vallarino <zvallarino@gmail.com>
2022-07-27 11:40:04 -0700

    working nav

---
+++ screens/EatScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const EatScreen = () => {
+  return (
+    <View>
+      <Text>EatScreen</Text>
+    </View>
+  )
+}
+
+export default EatScreen
